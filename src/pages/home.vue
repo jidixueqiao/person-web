c751f2a1d848bc60939b89c2f77507335ae8523f
高雲鹏 <gaoyunpeng@pivisys.com>
2025-01-22 18:17:06 +0800

    🌟feat: 添加animejs制作页面动效

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="home">
-    我是home页面
+    <div class="title" ref="titleRef">hello，我是极地雪橇</div>
   </div>
 </template>
+
+<script setup lang="ts">
+import anime from 'animejs/lib/anime.es.js';
+import { onMounted, shallowRef, ShallowRef } from 'vue';
+
+const titleRef:ShallowRef<HTMLDivElement | null> = shallowRef<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  anime({
+    targets: titleRef.value,
+    translateX: [50, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: 'easeInOutQuad',
+  })
+})
+</script>
